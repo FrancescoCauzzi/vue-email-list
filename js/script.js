b5fa33801021e4b410fb697072733fa516f8c049
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-03-22 13:27:29 +0100

    feat: esercizio base, done

--- js/script.js
+++ js/script.js
@@ -5,6 +5,20 @@ createApp({
   data() {
     return {
       message: "Hello Vue!",
+      myEmailArr: [],
+      emailAdressNumber: 10,
     };
+  },
+  created() {
+    for (let i = 0; i < this.emailAdressNumber; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((res) => {
+          console.log(res.data.response);
+          this.myEmailArr.push(res.data.response);
+
+          // lo assegno alla mia proprietà
+        });
+    }
   },
 }).mount("#app");
