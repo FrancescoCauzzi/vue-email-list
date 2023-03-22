9b441f98dbee88d87ab54798c71bfb7a12777b1e
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-03-22 16:26:27 +0100

    feat: soluzione con .get e .then

--- js/script.js
+++ js/script.js
@@ -9,17 +9,28 @@ createApp({
       emailAdressNumber: 10,
     };
   },
-  async created() {
-    try {
-      for (let i = 0; i < this.emailAdressNumber; i++) {
-        const response = await fetch(
-          "https://flynn.boolean.careers/exercises/api/random/mail"
-        );
-        const data = await response.json();
-        this.myEmailArr.push(data.response);
-      }
-    } catch (error) {
-      console.log(error);
+  created() {
+    for (let i = 0; i < this.emailAdressNumber; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((res) => {
+          console.log(res);
+          this.myEmailArr.push(res.data.response);
+        });
     }
   },
+
+  // async created() {
+  //   try {
+  //     for (let i = 0; i < this.emailAdressNumber; i++) {
+  //       const response = await fetch(
+  //         "https://flynn.boolean.careers/exercises/api/random/mail"
+  //       );
+  //       const data = await response.json();
+  //       this.myEmailArr.push(data.response);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // },
 }).mount("#app");
